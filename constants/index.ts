d077545f348f388b170cc837d845cb65baa0b991
Ndata <wisdomndata@gmail.com>
2025-05-25 08:14:15 +0100

    Rentalx

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const API_URL = 'https://api.example.com/properties';
+
+export const BUTTON_LABELS = {
+  BOOK_NOW: 'Book Now',
+  DETAILS: 'Details',
+};
---
+++ interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface CardProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
