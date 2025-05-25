a095a97fcf99b086f677009b6be81592eb4a9170
Ndata <wisdomndata@gmail.com>
2025-05-25 07:46:06 +0100

    ALX Listing App

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className=" text-center bg-[plum] w-[200px] h-[80px] flex justify-center items-center mx-auto">Wisdom</h1>
+    </div>
+  );
+}
